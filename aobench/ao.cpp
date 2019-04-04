b3088aa0d69ad5f1e9e6a6a014b92ea71e923d19
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2019-04-04 15:30:30 +0200

    Cleanup even more

--- aobench/ao.cpp
+++ aobench/ao.cpp
@@ -135,64 +135,24 @@ int main(int argc, char **argv)
     fimg = new float[width * height * 3];
     times = new double[maxIter];
 
-    //
-    // Run the ispc path, test_iterations times, and report the minimum
-    // time for any of them.
-    //
-    for (unsigned int i = 0; i < test_iterations[0]; i++) {
-        memset((void *)fimg, 0, sizeof(float) * width * height * 3);
-        assert(NSUBSAMPLES == 2);
+#define BENCH(iter, fn, res, name) \
+    for (unsigned int i = 0; i < iter; i++) { \
+        memset((void *)fimg, 0, sizeof(float) * width * height * 3); \
+        assert(NSUBSAMPLES == 2); \
+        reset_and_start_timer(); \
+        fn(width, height, NSUBSAMPLES, fimg); \
+        double t = get_elapsed_mcycles(); \
+        printf("@time of " name " run:\t\t\t[%.3f] million cycles\n", t); \
+        times[i] = t; \
+    } \
+    double res = median(times, iter); \
+    printf("[aobench " name "]:\t\t\t[%.3f] million cycles (%d x %d image)\n", res, width, height); \
+    savePPM("ao-" name ".ppm", width, height); \
 
-        reset_and_start_timer();
-        ao_ispc(width, height, NSUBSAMPLES, fimg);
-        double t = get_elapsed_mcycles();
-        printf("@time of ISPC run:\t\t\t[%.3f] million cycles\n", t);
-        times[i] = t;
-    }
-    double medISPC = median(times, test_iterations[0]);
-
-    // Report results and save image
-    printf("[aobench ispc]:\t\t\t[%.3f] million cycles (%d x %d image)\n", medISPC, width, height);
-    savePPM("ao-ispc.ppm", width, height);
-
-    //
-    // Run the AnyDSL path, test_iterations times, and report the
-    // minimum time for any of them.
-    //
-    for (unsigned int i = 0; i < test_iterations[1]; i++) {
-        memset((void *)fimg, 0, sizeof(float) * width * height * 3);
-        assert(NSUBSAMPLES == 2);
-
-        reset_and_start_timer();
-        ao_impala(width, height, NSUBSAMPLES, fimg);
-        double t = get_elapsed_mcycles();
-        printf("@time of AnyDSL run:\t\t\t[%.3f] million cycles\n", t);
-        times[i] = t;
-    }
-    double medAnyDSL = median(times, test_iterations[1]);
-
-    // Report results and save image
-    printf("[aobench AnyDSL]:\t\t[%.3f] million cycles (%d x %d image)\n", medAnyDSL, width, height);
-    savePPM("ao-anydsl.ppm", width, height);
-
-    //
-    // Run the serial path, again test_iteration times, and report the
-    // minimum time.
-    //
-    for (unsigned int i = 0; i < test_iterations[2]; i++) {
-        memset((void *)fimg, 0, sizeof(float) * width * height * 3);
-        reset_and_start_timer();
-        ao_serial(width, height, NSUBSAMPLES, fimg);
-        double t = get_elapsed_mcycles();
-        printf("@time of serial run:\t\t\t\t[%.3f] million cycles\n", t);
-        times[i] = t;
-    }
-    double medSerial = median(times, test_iterations[2]);
-
-    // Report more results, save another image...
-    printf("[aobench serial]:\t\t[%.3f] million cycles (%d x %d image)\n", medSerial, width, height);
-    printf("\t\t\t\t(%.2fx speedup from ISPC, %.2fx speedup from AnyDSL)\n", medSerial / medISPC, medSerial / medAnyDSL);
-    savePPM("ao-serial.ppm", width, height);
+    BENCH(test_iterations[0], ao_ispc,   timeISPC, "ispc")
+    BENCH(test_iterations[1], ao_impala, timeImpala, "impala")
+    BENCH(test_iterations[2], ao_serial, timeSerial, "serial")
+    printf("\t\t\t\t(%.2fx speedup from ISPC, %.2fx speedup from AnyDSL)\n", timeSerial / timeISPC, timeSerial / timeImpala);
 
     return 0;
 }
